f7418125a48bfbbe3fd9223c415a6e883cf4d817
cfoster121 <cfoster649@gmail.com>
2023-01-19 16:53:46 -0800

    Intro file covering python console basics

---
+++ app1/bonus1.py
@@ -0,0 +1,9 @@
+# text = input("Enter a title: ")
+# length = len(text)
+# print("Your title has", length, "characters")
+
+# variable = 'test test test'
+# print(type(variable))
+
+list = ['one', 'two', 'three', 'four']
+print(list)
