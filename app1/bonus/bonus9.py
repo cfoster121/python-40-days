f34f7739f5cff255ed670ac7300a02101add5074
cfoster121 <cfoster649@gmail.com>
2023-03-03 14:15:19 -0800

    Bonus9 comparing lists and dictionaries

---
+++ app1/bonus/bonus9.py
@@ -0,0 +1,34 @@
+password = input("Enter password: ")
+
+result = {}
+
+if len(password) >= 8:
+    result["length"] = True
+else:
+    result["length"] = False
+
+
+digit = False
+
+for i in password:
+    if i.isdigit():
+        digit = True
+
+result["digit"] = digit
+
+
+upper = False
+
+for i in password:
+    if i.isupper():
+        upper = True
+
+result["upper"] = upper
+
+
+print(result)
+
+if (all(result.values())) == True:
+    print("Strong password!!")
+else:
+    print("Weak password")
